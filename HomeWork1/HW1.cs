db9d8e794b6ac4fe14e42ef2a3fd99ecb6473e09
UserJul <yuliya.selezneva.1999@gmail.com>
2021-10-15 00:58:47 +0300

    all home work

---
+++ HomeWork1/HW1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+     public class HW1
+    {
+        public void SolveTask1()
+        {
+            int a = (int)GetNumberForUser("Введите число а");
+            int b = (int)GetNumberForUser("Введите число b");
+            double result = VariablesCulcFormula(a , b);
+            Console.WriteLine(result);
+        }
+        public void SolveTask2()
+        {
+            string stringOne = GetStringFromUser("Введите первую строку");
+            string stringTwo = GetStringFromUser("Введите вторую строку");
+            string result = ReverseStrings(stringOne, stringTwo);
+            Console.WriteLine($"Результат: {result}");
+
+        }
+        public void SolveTask3()
+        {
+            double a = GetNumberForUser("Введите число a");
+            double b = GetNumberForUser("Введите число b");
+            double result = DivideNumberswithoutRemainder(a, b);
+            double result1 = DivideNumberswithRemainder(a, b);
+            Console.WriteLine($"Результат третьей задачи: {result}, {result1}");
+
+
+        }
+        public void SolveTask4()
+        {
+            double a = GetNumberForUser("Введите число а");
+            double b = GetNumberForUser("Введите число b");
+            double c = GetNumberForUser("Введите число c");
+            double x = LinearEquationSolution(a, b, c);
+            Console.WriteLine($"x={x}");
+            
+        }
+        public void SolveTask5()
+        {
+            int x1 = (int)GetNumberForUser("Введите значение x1");
+            int x2 = (int)GetNumberForUser("Введите значение x2");
+            int y1 = (int)GetNumberForUser("Введите значение y1");
+            int y2 = (int)GetNumberForUser("Введите значение y2");
+            double A = EquationOfAStraightLine(x1, x2, y1, y2);
+            double B = FindFreeMember(y2, A, x2);
+            Console.WriteLine($"Y = {A}*X + {B} ");
+
+        }
+        public double VariablesCulcFormula(int a, int b)
+        {
+            double result = (5 * a + b * b) / (b - a);
+            return result;
+        }
+
+        public double GetNumberForUser(string messag)
+        {
+            Console.WriteLine(messag);
+            double number = Convert.ToDouble(Console.ReadLine());
+            return number;
+        }
+        public string GetStringFromUser(string message)
+        {
+            Console.WriteLine(message);
+            string stringOne = Console.ReadLine();
+            return stringOne;
+        }
+        public string ReverseStrings(string stringOne, string stringTwo)
+        {
+            string result = stringTwo + stringOne;
+            return result;
+        }
+        public double DivideNumberswithoutRemainder(double a, double b)
+        {
+            double result = a / b;
+            return result;
+
+        }
+        public double DivideNumberswithRemainder(double a, double b)
+        {
+            double result1 = a % b;
+            return result1;
+
+        }
+        public double LinearEquationSolution( double a, double b, double c)
+        {
+            double x = (c - b) / a;
+            return x;
+        }
+        public double EquationOfAStraightLine(double x1, double x2, double y1, double y2)
+        {
+            double a = (y2 - y1) / (x2 - x1);
+            return a;
+        }
+        public static double FindFreeMember(double x2, double y2, double a)
+        {
+            double b = (y2 - (a * x2));
+            return b;
+        }
+    }
+}
---
+++ TheAllHomeWoks/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using HomeWork1;
+using HomeWork2;
+using HomeWork3;
+using HomeWork4;
+using HomeWork5;
+
+namespace TheAllHomeWoks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HW1 hw1 = new HW1();
+            HW2 hw2 = new HW2();
+            HW3 hw3 = new HW3();
+            HW4 hw4 = new HW4();
+            HW5 hw5 = new HW5();
+
+            // hw1.SolveTask1();
+            // hw1.SolveTask2();
+            //hw1.SolveTask3();
+            //hw1.SolveTask4();
+            //hw1.SolveTask5();
+
+            //hw2.SolveTask1();
+            //hw2.SolveTask2();
+            //hw2.SolveTask3();
+            //hw2.SolveTask4();
+            //hw2.SolveTask5();
+
+            //hw3.SolveTask1();
+            //hw3.SolveTask2();
+            //hw3.SolveTask3();
+            //hw3.SolveTask4();
+            //hw3.SolveTask5(); 
+            //hw3.SolveTask6();
+        //hw3.SolveTask7();
+          // hw3.SolveTask8();
+        //   hw3.SolveTask9();
+         //  hw3.SolveTask10();
+          //hw3.SolveTask11();
+            hw3.SolveTask12();
+        }
+    }
+}
